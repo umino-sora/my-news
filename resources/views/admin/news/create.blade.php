4d1ee1451c5f800bd620a3d6421e2c3bff89d624
shimada <ue113241@gmail.com>
2020-08-25 08:57:08 +0000

    教材20 完了

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -11,3 +11,17 @@
         <h1>Myニュース作成画面</h1>
     </body>
 </html>
+
+@extends('layouts.admin')
+
+@section('title', 'ニュースの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>ニュース新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
