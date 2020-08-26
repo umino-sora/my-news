fc7b4ea4f7125259d2928fc53b259eef0ab26fc9
shimada <ue113241@gmail.com>
2020-08-26 03:15:21 +0000

    課題20 提出

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<!-- 課題4 -->
+
 <html>
     <head>
         <meta charset="utf-8">
@@ -11,3 +13,17 @@
         <h1>Myプロフィール作成画面</h1>
     </body>
 </html>
+
+@extends('layouts.profile')
+
+@section('title', 'プロフィールの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
