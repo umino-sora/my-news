5247aee88a0f66ad818fe5d9f235a22539bdb0a6
shimada <ue113241@gmail.com>
2020-08-26 08:37:08 +0000

    教材21 完了

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 //課題3
@@ -25,3 +25,7 @@ Route::group(['prefix' => 'admin'], function() {
 //課題4
 Route::get('admin/profile/create', 'Admin\ProfileController@add');
 Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
