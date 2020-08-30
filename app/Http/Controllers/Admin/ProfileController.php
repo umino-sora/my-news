98dc05fc81ebc9d160315c0fb4242e7e7a7a3fcb
shimada <ue113241@gmail.com>
2020-08-30 03:28:30 +0000

    課題23 提出

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,20 +1,30 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Plofile;
+
 class ProfileController extends Controller
 {
-    //
     public function add()
     {
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Plofile::$rules);
+        
+        $plofiel = new Plofile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
+        $plofiel->fill($form);
+        $plofiel->save();
+        
         return redirect('admin/profile/create');
     }
     
