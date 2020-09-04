eecd56c074f4b483d1eb0ee634400f941914d53f
shimada <ue113241@gmail.com>
2020-09-04 05:13:38 +0000

    教材26 完了

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/edit', 'Admin\ProfileController@update');
     Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    route::get('news/delete', 'Admin\NewsController@delete');
 });
 
 Auth::routes();
