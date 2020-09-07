f6b0c5138cc49cc1905556463bafcddcbf96e82b
shimada <ue113241@gmail.com>
2020-09-07 08:08:46 +0000

    教材29 完了

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,4 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'NewsController@index');
